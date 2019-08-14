853679570178bc6bbbf1717239d3205286e56794
Fritz O. Yohn III <i20554@verisk.com>
2019-08-14 11:43:11 -0400

    added code to test

--- Chapter_12/Practice/Wikipedia1.py
+++ Chapter_12/Practice/Wikipedia1.py
@@ -1,1 +1,15 @@
 #try to connect to wikipedia and grab the text off a webpage
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('en.wikipedia.org', 80))
+cmd = 'GET https://en.wikipedia.org/wiki/16-bit\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
