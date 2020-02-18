92e0b4e3412313c306ff9182b42ac162c93ce05d
Fritz O. Yohn III <i20554@verisk.com>
2020-02-18 16:46:48 -0500

    Chapter 2

---
+++ Chapter_2/Assignment_2_3.py
@@ -0,0 +1,7 @@
+Hrs = input("Enter Hours:")
+Hrs_float = float(Hrs)
+Rate = input("Enter Rate:")
+Rate_Float= float(Rate)
+Pay = Hrs_float*Rate_Float
+Pay_string = str(Pay)
+print("Pay: "+Pay_string)
