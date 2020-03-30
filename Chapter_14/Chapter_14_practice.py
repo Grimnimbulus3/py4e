cf1de37cc17450d305d9b9acd1c1d85bfec93448
Fritz O. Yohn III <i20554@verisk.com>
2020-03-29 21:21:20 -0400

    chapt14_practice

---
+++ Chapter_14/Chapter_14_practice.py
@@ -0,0 +1,21 @@
+#practice for chapter 14, and Week 1 of the fourth course
+
+#ASCII character set
+
+#ord function gets the ascii number out
+print(ord('H'))
+print(ord('e'))
+print(ord('\n'))
+
+##UTF =Unicode
+#UTF-16 two bytes
+#UTF-32 four bytes
+#UTF-8 1-4 bytes  RECOMMENDED AND AUTO DETECTIION W/ ASCII
+
+#14.1 Object Oreinted Definitions and terminology
+
+#an OBJECT is a self-contained piece of code and data
+#Class - a template or blueprints for objects
+#Method or Message:  A definied capability of class
+#Field or attribute: A bit of data in a class
+#Object or Instance: A particular instance of a class
