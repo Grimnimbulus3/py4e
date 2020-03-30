4171a1cae874f305a7f1eaa708c40c690e5adc3e
Grimnimbulus <Nimbulus99@gmail.com>
2020-03-29 22:03:23 -0400

    Chapter_14_1

--- Chapter_14/Chapter_14_practice.py
+++ Chapter_14/Chapter_14_practice.py
@@ -19,3 +19,42 @@ print(ord('\n'))
 #Method or Message:  A definied capability of class
 #Field or attribute: A bit of data in a class
 #Object or Instance: A particular instance of a class
+
+#14.2 Classes: our first class and objexts
+class PartyAnimal:
+     x = 0
+     def party(self):
+         self.x = self.x + 1
+         print(" So far", self.x)
+an = PartyAnimal()
+an.party()
+an.party()
+an.party()
+PartyAnimal.party(an)
+print ("Type", type(an))
+print ("Dir ", dir(an))
+print ("Type", type(an.x))
+print ("Type", type(an.party))
+
+#14.3: Object life cycle
+
+#constructor sets up a set of initial values
+#destrucot is optional and trips when the object is destroyed
+
+#Many instances
+class PartyAnimal:
+    x = 0
+    name = ''
+    def __init__(self, nam):
+          self.name = nam
+          print( self.name,'constructed')
+    def party(self):
+        self.x = self.x + 1
+        print(self.name,'party count', self.x)
+s = PartyAnimal('Sally')
+j = PartyAnimal('Jim')
+s.party()
+j.party()
+s.party()
+
+#14.4 object inheritance
